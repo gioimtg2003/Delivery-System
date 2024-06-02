a829b1bd683ebcfff7a4acaf11613a4280c5b566
gioimtg2003 <conggioi.pro264@gmail.com>
2024-06-02 14:41:26 +0700

    feat: sign url s3

---
+++ server/src/Services/AWS/BaseSimpleStorage.ts
@@ -0,0 +1,31 @@
+import { PutObjectCommand, S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
+import { AWS_CONFIG } from "../../Configs/aws";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+export class BaseSimpleStorage {
+    protected s3Client: S3Client;
+    protected s3ClientConfig: S3ClientConfig;
+    constructor() {
+        this.s3ClientConfig = {
+            region: AWS_CONFIG.region,
+            credentials: {
+                accessKeyId: AWS_CONFIG.accessKeyId as string,
+                secretAccessKey: AWS_CONFIG.secretAccessKey as string,
+            },
+            // endpoint: AWS_CONFIG.endpoint, // prod
+        };
+        this.s3Client = new S3Client(this.s3ClientConfig);
+    }
+
+    public getPresignedUrl({
+        bucket,
+        key,
+        exp,
+    }: {
+        readonly bucket: string;
+        readonly key: string;
+        readonly exp: number;
+    }) {
+        const command = new PutObjectCommand({ Bucket: bucket, Key: key });
+        return getSignedUrl(this.s3Client, command, { expiresIn: exp });
+    }
+}
